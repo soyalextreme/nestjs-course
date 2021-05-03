c7fe0d5a785cb9b476339bc502f519c5dde27f32
Alejandro AS <soyalextreme@gmail.com>
2021-05-02 22:23:14 -0500

    Adding some comments

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -26,8 +26,9 @@ export class ProductsController {
     // dynamic endpoints
 
     @Get(':id')
-    @HttpCode(HttpStatus.ACCEPTED)
+    @HttpCode(HttpStatus.ACCEPTED) // using decorator
     getOne(@Res() response: Response, @Param('id') id: string) {
+        // using express directly
         response.status(200).send({
             id,
         });
