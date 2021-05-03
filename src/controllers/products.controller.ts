1111353eb7d7895cc8f150a1cb7bb7f95e8fb4fd
Alejandro AS <soyalextreme@gmail.com>
2021-05-02 21:31:05 -0500

    Class Put Delete - Add method Put - Add method delete for the products using decorators.

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Query, Post, Body, Put, Delete } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -41,4 +41,21 @@ export class ProductsController {
             payload,
         };
     }
+
+    @Put(':productId')
+    update(@Param('productId') id: number, @Body() payload: any) {
+        return {
+            id,
+            payload,
+        };
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: number) {
+       return {
+           id,
+       } 
+    }
+    
+
 }
